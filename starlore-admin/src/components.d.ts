3048b7b5d7f59786be9f6ed48c7ae6d87d4eb503
xinke <872709652@qq.com>
2026-09-17 22:14:07 +0800

    style(home): 去除首页 AI 伴侣小球嵌套卡片包裹样式

--- starlore-admin/src/components.d.ts
+++ starlore-admin/src/components.d.ts
@@ -12,7 +12,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AppModal: typeof import('./components/common/AppModal.vue')['default']
-    AppSidebar: typeof import('./components/business/AppSidebar.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
